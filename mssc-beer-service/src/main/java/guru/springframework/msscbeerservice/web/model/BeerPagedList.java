0214339385a4ab15130ca11afbc3db80a3e21a24
Nguyen Thanh Nam <thanhnam192@gmail.com>
2020-04-17 08:22:40 +0700

    client
    client

---
+++ mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/web/model/BeerPagedList.java
@@ -0,0 +1,18 @@
+package guru.springframework.msscbeerservice.web.model;
+
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class BeerPagedList extends PageImpl<BeerDto> implements Serializable {
+
+    public BeerPagedList(List<BeerDto> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+
+    public BeerPagedList(List<BeerDto> content) {
+        super(content);
+    }
+}
