6f91acfc2a9b090725642751d0c2a045555c5418
Nguyen Thanh Nam <thanhnam192@gmail.com>
2020-04-30 13:49:32 +0700

    remove unused
    remove unused

--- mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/repositories/BeerRepository.java
+++
@@ -1,9 +0,0 @@
-package guru.springframework.msscbeerservice.repositories;
-
-import guru.springframework.msscbeerservice.domain.Beer;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import java.util.UUID;
-
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
-}
